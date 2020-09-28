04ffa5d397af1273d961ca2527216f872d2c61b5
Cole Chapman <chapm110@mail.nmc.edu>
2020-09-28 10:39:53 -0400

    Add project files.

---
+++ MakeAMethod/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MakeAMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+
+            age = DisplayGetUserAge();
+        }
+
+        static int DisplayGetUserAge()
+        {
+            int age;
+            string userResponse;
+            bool validResponse;
+
+            do
+            {
+                validResponse = true;
+                Console.WriteLine("Enter age in years");
+                
+                //userResponse = Console.ReadLine();
+                //if (!int.TryParse(userResponse,out age))
+                //{
+                //    Console.WriteLine("Please enter your age as an integer");
+                //    validResponse = false;
+                //}
+
+                if (!int.TryParse(Console.ReadLine(), out age))
+                {
+                    Console.WriteLine("Please enter your age as an integer");
+                    validResponse = false;
+                }
+            } while (!validResponse);
+
+            return age;
+            
+        }
+    }
+}
